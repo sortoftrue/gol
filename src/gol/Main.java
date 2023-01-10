61c602677046d03998859ebf0b01bdaf5b381c0b
himynameisjamestay@gmail.com <james>
2023-01-10 22:29:57 +0800

    gol complete

--- src/gol/Main.java
+++ src/gol/Main.java
@@ -15,13 +15,13 @@ public class Main{
         char[][] startGrid = null;
 
         try {
-            startGrid = setupGrid();
+            startGrid = setupGrid("glider.gol");
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
-        //output grid
+        //output startgrid
         for(int i=0; i<startGrid.length;i++){
             for(int j=0;j<startGrid[0].length;j++){
                 System.out.print("[" + startGrid[i][j] + "]");
@@ -29,22 +29,103 @@ public class Main{
             System.out.println();
         }
 
+        for(int k=1;k<=100;k++){
+            char[][] nextGrid = evalNext(startGrid);
+        
+            //output nextGrid
+            System.out.printf("Run: %d\n",k);
+            for(int i=0; i<nextGrid.length;i++){
+                for(int j=0;j<nextGrid[0].length;j++){
+                    System.out.print("[" + nextGrid[i][j] + "]");
+                }
+                System.out.println();
+            }
+            startGrid = nextGrid;
+            System.out.println();
+        }
+        
+
     }
 
     public static char[][] evalNext(char[][] currentGrid){
-        char[][] nextGrid;
+        
+        int ySize = currentGrid.length;
+        int xSize = currentGrid[0].length;
+        char[][] nextGrid = new char[ySize][xSize];
         int neighbours = 0;
 
+        for(int i=0;i<ySize;i++){
+            for (int j=0;j<xSize;j++){
+                try{
+                    if(currentGrid[i-1][j-1]=='*'){
+                    neighbours++;
+                    }
+                } catch (Exception e){}
+                try{
+                    if(currentGrid[i-1][j]=='*'){
+                    neighbours++;
+                    }
+                } catch (Exception e){}
+                try{
+                    if(currentGrid[i-1][j+1]=='*'){
+                    neighbours++;
+                    }
+                } catch (Exception e){}
+                try{
+                    if(currentGrid[i][j-1]=='*'){
+                    neighbours++;
+                    }
+                } catch (Exception e){}
+                try{
+                    if(currentGrid[i][j+1]=='*'){
+                    neighbours++;
+                    }
+                } catch (Exception e){}
+                try{
+                    if(currentGrid[i+1][j-1]=='*'){
+                    neighbours++;
+                    }
+                } catch (Exception e){}
+                try{
+                    if(currentGrid[i+1][j]=='*'){
+                    neighbours++;
+                    }
+                } catch (Exception e){}
+                try{
+                    if(currentGrid[i+1][j+1]=='*'){
+                    neighbours++;
+                    }
+                } catch (Exception e){}
+                
+                if (neighbours <=1){
+                    nextGrid[i][j] = ' ';
+                } else if(neighbours == 2){
+                    if (currentGrid[i][j]=='*'){
+                        nextGrid[i][j] = '*';
+                    } else nextGrid[i][j] = ' ';
+                } else if(neighbours ==3){
+                    nextGrid[i][j] = '*';
+                } else if(neighbours >=3){
+                    nextGrid[i][j] = ' ';
+                }
+
+                //nextGrid[i][j]=(char)(neighbours+'0');
+                neighbours =0;
+
+
+            }
+        }
+        
 
 
         return nextGrid;
     }
 
-    public static char[][] setupGrid() throws IOException{
+    public static char[][] setupGrid(String filename) throws IOException{
         char[][] startgrid=null;
 
         //readfile
-        File golfile = new File("golfiles/blinker.gol");
+        File golfile = new File("golfiles/%s".formatted(filename));
         BufferedReader reader = new BufferedReader(new FileReader(golfile));
         String readLine = null;
         int xSize;
